f45cdac6d2756197b12cadf3e93baa8ae33c22ff
mkorczynska <k.martyna95@gmail.com>
2019-10-06 22:16:52 +0200

    ..

--- atergo.py
+++ atergo.py
@@ -1,23 +1,22 @@
-import codecs, re, csv, locale
+import locale
+import re
 
 
 def words_from_line(line):
-        words=re.split('[\W\d]+',line)
-        return [w.lower() for w in words if w]
+    words = re.split('[\W\d]+', line)
+    return [w.lower() for w in words if w]
 
 
-file="wiki.txt"
-dictionary={}
+file = "wiki.txt"
+dictionary = {}
 
 with open(file, 'r', encoding="utf-8") as txtfile:
-        for line in txtfile:
-                words=words_from_line(line)
-                for word in words:
-                       revword=word[::-1]
-                       if word not in dictionary:
-                              dictionary[revword]=word
+    for line in txtfile:
+        words = words_from_line(line)
+        for word in words:
+            revword = word[::-1]
+            if word not in dictionary:
+                dictionary[revword] = word
 
-for word in sorted(dictionary, key=locale.strxfrm):# key=dictionary.get):
-        print('%35s %s'%(dictionary[word], word))
-
-
+for word in sorted(dictionary, key=locale.strxfrm):  # key=dictionary.get):
+    print('%35s %s' % (dictionary[word], word))
